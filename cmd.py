14a2f15fc96b69be167984121a3b4f89228ce5f2
ayushi-28 <ayushijp@gmail.com>
2018-05-24 13:05:37 +0530

    cmd.py

---
+++ cmd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+import cgi #get data from cgi
+import commands
+
+print "Content-type:text/html"  #the type of content will be html
+print "" #print space 
+
+web_data=cgi.FieldStorage()
+#to extract only the necessary info,the data sent by client.Here x is the actual variable in the link in which data is stored
+result=commands.getoutput('date')
+
+out= web_data.getvalue('x') 
+print out
+print"<h1>"
+print "<marquee>"
+print result
+print "</marquee>"
+print"</h1>"
+
+#<pre> this tag is used for proper orientation 
+#chmod +x cmd.py
